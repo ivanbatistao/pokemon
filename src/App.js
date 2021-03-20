38aaa2c5f68f78616b65db48f0e33e4eafee4c14
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-19 22:46:34 -0500

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+import Home from "./components/Home"
+import NavBar from "./components/NavBar"
+import Cards from "./components/Cards"
+import MoreInfo from "./components/MoreInfo"
+
+import { Route } from "react-router-dom"
 
 function App() {
+  const [pokemones, setPokemones] = useState([])
+
+  function onSearch(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((r) => r.json())
+      .then((resource) => {
+        // console.log(resource)
+        if (resource.abilities !== undefined) {
+          const pokemon = {
+            name: resource.name,
+            experience: resource.base_experience,
+            id: resource.id,
+            firstAbility: resource.abilities[0].ability.name,
+            attack: resource.base_experience,
+          }
+          setPokemones((oldPokemones) => [...oldPokemones, pokemon])
+        } else {
+          alert("Pokemon Not Found")
+        }
+      })
+  }
+
+  console.log(pokemones)
+
+  function onClose(id) {
+    setPokemones((oldPokemones) => oldPokemones.filter((p) => p.id !== id))
+  }
+
+  function onFilter(pokemonId) {
+    let poke = pokemones.filter(p => p.id === parseInt(pokemonId))
+    console.log(poke)
+    if (poke.length > 0) {
+      return poke[0]
+    } else {
+      return null
+    }
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Home} />
+      <Route path="/pokemones">
+        <header className="App-header">
+          <NavBar onSearch={onSearch} />
+        </header>
+      </Route>
+      <Route
+        exact
+        path="/pokemones/:pokemonId"
+        render={({ match }) => 
+          <MoreInfo moreInfo={onFilter(match.params.pokemonId)} />
+        }
+      />
+      <Route
+        exact
+        path="/pokemones"
+        render={() => <Cards pokemones={pokemones} onClose={onClose} />}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
